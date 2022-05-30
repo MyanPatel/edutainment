ac6db6c768f30570fb6f18d89207976475f872e6
Myan Patel <myanpatel@MacBook-Pro.local>
2022-05-30 23:18:44 +0100

    Initial Commit

---
+++ edutainment/edutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  edutainmentApp.swift
+//  edutainment
+//
+//  Created by Myan Patel on 30/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct edutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
